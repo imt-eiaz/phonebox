2f754bc791630d32fd4e4937714630196d3a9b64
Imtiaz Khan <imteiaz@gmail.com>
2025-02-22 17:27:09 +0000

    localStorage Creation TS 3:00 of video

--- app/iphone/page.js
+++ app/iphone/page.js
@@ -1,7 +1,26 @@
-import React from "react";
+"use client"; // This is a client component 👈🏽
+
+import React, { useState } from "react";
 
 const Iphone = () => {
-  return <div>iPhone Page CHANGED</div>;
+  const [favNumber, setFavNumber] = useState();
+
+  return (
+    <>
+      <div>
+        <h1>iPhone Page CHANGED</h1>
+        <a href="/">Go to home</a>
+      </div>
+      <form>
+        <input
+          type="number"
+          value={favNumber}
+          onChange={(e) => setFavNumber(e.target.value)}
+        ></input>
+        <input type="submit" value="Save"></input>
+      </form>
+    </>
+  );
 };
 
 export default Iphone;
